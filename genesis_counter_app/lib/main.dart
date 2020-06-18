44b307e097e4b7d24c8ab309778f348ff45d78b5
ganeshbhandarkar <bhandarkarganesh8181@gmail.com>
2020-06-18 09:18:04 +0530

    Task Completed

---
+++ genesis_counter_app/lib/main.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+
+  // Status Bar Color
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    systemNavigationBarColor: Color(0xfffbb034),
+    statusBarColor: Colors.orange[600],
+  ));
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startwithgenesis',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        accentColor: Color(0xfffbb034),
+        primaryColor: Color(0xfffbb034),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+//      if(_counter!=0){
+      _counter--;
+//      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "STARTWITHGENESIS",
+          style: TextStyle(fontSize: 15, letterSpacing: 4),
+        ),
+        leading: Icon(
+          Icons.format_shapes,
+          color: Colors.red,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25),
+        ),
+        elevation: 0,
+        centerTitle: true,
+        titleSpacing: 5,
+      ),
+      body: Stack(
+        children: <Widget>[
+          Positioned.fill(
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ),
+          ),
+          Positioned.fill(
+            top: 70,
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                "Counter",
+                style: Theme.of(context).textTheme.overline,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: FloatingActionButton(
+                elevation: 0,
+                onPressed: _decrementCounter,
+                tooltip: 'Decrement',
+                child: Icon(Icons.remove),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: FloatingActionButton(
+                elevation: 0,
+                onPressed: _incrementCounter,
+                tooltip: 'Increment',
+                child: Icon(Icons.add),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
